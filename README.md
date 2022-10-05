ee2e7b521c9616e206698465894b3fa379f8b7d8
Keli <krsparks2@yahoo.com>
2022-10-04 19:54:58 -0700

    Readme updates

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # 06-Challenge-weather-app
+
+## Description
+This application allows the user to input any city to search for the current weather conditions. 
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Questions](#questions)
+- [Visuals](#visuals)
+
+## Installation
+
+This is run in the browser
+
+## Usage
+
+I used a third party API to pull in the data
+
+## Contributing
+
+[Michael Montoya](https://github.com/mmontoya1112)
+
+## Credits
+
+[OpenWeather One Call API](https://openweathermap.org/api/one-call-api)
+
+## Questions
+
+[Keli-Rene Sparks](https://www.github.com/tracyroseguajardo)
+
+## Visuals
+
+Link to deployed application:
+[Weather Dashboard](https://tracyroseguajardo.github.io/weather-dashboard/)
+
+Screenshot of application:
+![Weather Dashboard Screenshot](./assets/images/weather-dashboard-screenshot.png)
--- script.js
+++ script.js
@@ -18,14 +18,14 @@ let weather =
         const { speed } = data.wind;
         console.log(name, icon, description, temp, humidity, speed)
         document.querySelector(".city").innerText = "Weather in " + name;
-        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@1x.png";
         document.querySelector(".description").innerText = "desciption: " + description;
         document.querySelector(".temp").innerText = "temperature: " + temp + "°F";
         document.querySelector(".humidity").innerText = "humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "wind speed " + speed + "mph";
         },
         search: function (){
-            this.fetchWeatherAPI(document.querySelector(".searchBar").value);
+            this.fetchWeather(document.querySelector(".searchBar").value);
         }
 };
 
