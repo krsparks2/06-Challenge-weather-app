03c536634ce545d8c0a6d169215090785f48bf78
Keli <krsparks2@yahoo.com>
2022-10-04 19:00:46 -0700

    looks good but doesn't work

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 let weather =
 {
-    "apiKey": "6b78723687548fe008cc1fed9057aafb"
-}
+    "apiKey": "6b78723687548fe008cc1fed9057aafb",
+    fetchWeather: function(city){
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            + "&units=imperial&appid=" 
+            + this.apiKey
+        )
+            .then((response) => response.json())
+            .then((data) => this.weatherInfo(data));
+    },
+    weatherInfo: function (data){
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed)
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon;
+        document.querySelector(".description").innerText = "desciption: " + description;
+        document.querySelector(".temp").innerText = "temperature: " + temp + "°F";
+        document.querySelector(".humidity").innerText = "humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "wind speed " + speed + "mph";
+        },
+        search: function (){
+            this.fetchWeatherAPI(document.querySelector(".searchBar").value);
+        }
+};
+
+document.querySelector(".search button").addEventListener("click", function() {
+    weather.search();
+    })
